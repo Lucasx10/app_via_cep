c60c420fd3c051611a785860bb30725833cd9b66
Lucas Prado <lukasprado260@gmail.com>
2023-10-15 16:05:09 -0400

    Add Icon no app e pacote ConvexAppBar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_via_cep/pages/ceps_cadastrados_page.dart';
 import 'package:app_via_cep/pages/home_page.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -9,44 +10,54 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
-  PageController controller = PageController(initialPage: 0);
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+  }
+
   int posicaoPagina = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: PageView(
-                controller: controller,
-                onPageChanged: (value) {
-                  setState(() {
-                    posicaoPagina = value;
-                  });
-                },
-                children: const [
-                  MyHomePage(),
-                  ListaCepCadastradoPage(),
-                ],
+          body: Column(
+            children: [
+              Expanded(
+                child: TabBarView(
+                  controller: tabController,
+                  children: const [
+                    MyHomePage(),
+                    ListaCepCadastradoPage(),
+                  ],
+                ),
               ),
-            ),
-            BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                onTap: (value) {
-                  controller.jumpToPage(value);
-                },
-                currentIndex: posicaoPagina,
-                items: const [
-                  BottomNavigationBarItem(
-                      label: "Home", icon: Icon(Icons.home)),
-                  BottomNavigationBarItem(
-                      label: "Ceps Cadastrados", icon: Icon(Icons.edit_note_sharp)),
-                ])
-          ],
-        ),
-      ),
+            ],
+          ),
+          // bottomNavigationBar: BottomNavigationBar(
+          //     type: BottomNavigationBarType.fixed,
+          //     onTap: (value) {
+          //       tabController.jumpToPage(value);
+          //     },
+          //     currentIndex: posicaoPagina,
+          //     items: const [
+          //       BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          //       BottomNavigationBarItem(
+          //           label: "Ceps Cadastrados",
+          //           icon: Icon(Icons.edit_note_sharp)),
+          //     ])
+          bottomNavigationBar: ConvexAppBar(
+            items: [
+              TabItem(icon: Icons.home, title: 'Home'),
+              TabItem(icon: Icons.map, title: 'Ceps Cadastrados'),
+            ],
+            onTap: (int i) => tabController.index = i,
+            controller: tabController,
+          )),
     );
   }
 }
